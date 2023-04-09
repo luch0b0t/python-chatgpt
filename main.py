8734665a0ea9b0013330a05f721895fe9a282ef1
Luis  Sotillo <luis.e.sotillo@gmail.com>
2023-04-09 14:10:23 -0400

    give initial context

--- main.py
+++ main.py
@@ -5,21 +5,32 @@ import openai
 
 dotenv.load_dotenv()
 
+def new_content(role: str, content: str):
+    return {"role": role, "content": content}
+
 def main():
 
 
     openai.organization = os.environ.get('OPENAPI_ORGANIZATION')
     openai.api_key = os.environ.get('OPENAI_KEY')
+    messages = []
 
-    user_content = input("Que deseas preguntar?: ")
+    system_context = new_content("system", "Eres un asistente muy útil.")
 
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                 messages=[{
-                                     "role": "user", "content": user_contentcual
-                                 }])
+    messages.append(system_context)
 
-    print(response.choices[0].message.content)
+    while True:
+        content = input("Que deseas preguntar?: ")
+        if content == "exit":
+            return None
 
+        user_content = new_content("user", content)
+        messages.append(user_content)
+
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                     messages=messages)
+
+        print(response.choices[0].message.content)
 
 
 if __name__ == '__main__':
