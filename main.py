7992f79c685f93d1e34ea3011b7dd342d5397f89
Luis  Sotillo <luis.e.sotillo@gmail.com>
2023-04-09 15:25:26 -0400

    add table and print colors

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import dotenv
 import os
 import openai
+import typer
+from rich import print
+from rich.table import Table
 
 
 dotenv.load_dotenv()
 
 def new_content(role: str, content: str):
     return {"role": role, "content": content}
+
+
+def add_context(role: str, content: str, messages: list):
+    content = new_content(role, content)
+    return messages.append(content)
+
+
+def __prompt() -> str:
+    prompt = typer.prompt("Que deseas preguntar?: ")
+    if prompt == "exit":
+        raise typer.Exit()
+
+    return prompt
 
 def main():
 
@@ -15,28 +31,35 @@ def main():
     openai.api_key = os.environ.get('OPENAI_KEY')
     messages = []
 
-    system_context = new_content("system", "Eres un asistente muy útil.")
+    print("[bold blue] Welcome to this ChatGPT API implementation [/bold blue]")
 
-    messages.append(system_context)
+    add_context("system", "Eres un asistente muy útil.", messages)
+
+    table = Table("Comando", "Descripción")
+    table.add_row("exit", "Salir de la aplicación")
+    table.add_row("new", "Crear una nueva conversación")
+
+    print(table)
 
     while True:
-        content = input("Que deseas preguntar?: ")
-        if content == "exit":
-            return None
+        content = __prompt()
 
-        user_content = new_content("user", content)
-        messages.append(user_content)
+        if content == "new":
+            messages = []
+            add_context("system", "Eres un asistente muy útil.", messages)
+            break
+
+        add_context("user", content, messages)
 
         response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
                                      messages=messages)
 
         response_context = response.choices[0].message.content
-        print(response.choices[0].message.content)
+        print(f"[green]>[/green]  {response.choices[0].message.content} \n"
+              f"----------------------------------------------------------")
 
-
-        asistant_context = new_content("assistant", response_context)
-        messages.append(asistant_context)
+        add_context("assistant", response_context, messages)
 
 
 if __name__ == '__main__':
-    main()
+    typer.run(main)
