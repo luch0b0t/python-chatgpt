ec1268d60fd3115aace99af6c6ab9751b4f64dc8
Luis  Sotillo <luis.e.sotillo@gmail.com>
2023-04-09 11:43:11 -0400

    define model gpt-3.5

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import os
 import openai
 
 
+dotenv.load_dotenv()
+
 def main():
 
     openai.organization = "org-MNpGtwNUecIdgArFyfvz6qVs"
     openai.api_key = os.environ.get('OPENAI_KEY')
+
+    openai.ChatCompletion.create(model="gpt-3.5-turbo")
+
+
 
 
 if __name__ == '__main__':
