8d85310f870230618bce7bb4318a63044b805fd8
Luis  Sotillo <luis.e.sotillo@gmail.com>
2023-04-09 12:23:05 -0400

    user ask

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ dotenv.load_dotenv()
 
 def main():
 
-    openai.organization = "org-MNpGtwNUecIdgArFyfvz6qVs"
+
+    openai.organization = os.environ.get('OPENAPI_ORGANIZATION')
     openai.api_key = os.environ.get('OPENAI_KEY')
+
+    user_content = input("Que deseas preguntar?: ")
 
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
                                  messages=[{
-                                     "role": "user", "content": "Cual esla vision de OpenAI?"
+                                     "role": "user", "content": user_contentcual
                                  }])
 
     print(response.choices[0].message.content)
